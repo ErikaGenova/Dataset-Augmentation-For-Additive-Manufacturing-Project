6defa1c2fffd8447fd536cb4afb1e5cbcd927db4
ponzuoligiacomo@gmail.com <ponzuoligiacomo@gmail.com>
2025-04-26 17:56:31 +0200

    fix

--- src/train.py
+++ src/train.py
@@ -39,7 +39,7 @@ def train_one_fold(train_idx, val_idx, file_paths, labels, device, args, fold):
     
     # Freeze all layers except the last 2 layers
     for name, param in model.named_parameters():
-        if "layer2" in name or "layer3" in name or "layer4" in name or "fc" in name:
+        if "layer3" in name or "layer4" in name or "fc" in name:
             param.requires_grad = True
         else:
             param.requires_grad = False
@@ -48,7 +48,7 @@ def train_one_fold(train_idx, val_idx, file_paths, labels, device, args, fold):
     optimizer = optim.Adam(
         filter(lambda p: p.requires_grad, model.parameters()), 
         lr=args.lr,
-        weight_decay=1e-4 # L2 regularization
+        weight_decay=1e-3 # L2 regularization
     )
 
     best_val_acc = 0.0
