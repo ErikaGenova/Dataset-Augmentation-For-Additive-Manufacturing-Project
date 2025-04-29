073e42561a461169d356cdb94c3d70a499787d78
ponzuoligiacomo@gmail.com <ponzuoligiacomo@gmail.com>
2025-04-29 10:28:45 +0200

    update dir

--- SinGAN/SinGAN/functions.py
+++ SinGAN/SinGAN/functions.py
@@ -22,7 +22,8 @@ from sklearn.cluster import KMeans
 """
 def read_image(opt):
     # Read the image from the specified input directory and name
-    x = img.imread('%s%s' % (opt.input_img,opt.ref_image))
+    print("Reading image from %s%s" % (opt.input_dir,opt.input_name))
+    x = img.imread('%s%s' % (opt.input_dir,opt.ref_image))
 
     # If the image is grayscale (2D), add a channel dimension
     return np2torch(x)
