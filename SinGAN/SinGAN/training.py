7b8ea7ce3a71efddc2fa26756115cb4394ca25b3
ponzuoligiacomo@gmail.com <ponzuoligiacomo@gmail.com>
2025-05-01 12:15:28 +0200

    finetuning for generator

--- SinGAN/SinGAN/training.py
+++ SinGAN/SinGAN/training.py
@@ -89,11 +89,11 @@ def train(opt, Gs, Zs, reals, NoiseAmp):
 """
 def train_single_scale(netD, netG, reals, Gs, Zs, in_s, NoiseAmp, opt, centers=None):
     # Set the parameters for the training process
-    opt.lr_d = 0.0001  # Riduci il tasso di apprendimento del discriminatore
+    opt.lr_d = 0.00005  # Riduci il tasso di apprendimento del discriminatore
     opt.lr_g = 0.0015  # Aumenta il tasso di apprendimento del generatore
 
     # Set the number of iterations and steps for the generator and discriminator
-    opt.Gsteps = 6  # Aumenta il numero di passi del generatore
+    opt.Gsteps = 8  # Aumenta il numero di passi del generatore
     opt.Dsteps = 1  # Riduci il numero di passi del discriminatore
 
     # Image preprocessing and network configuration
@@ -249,7 +249,8 @@ def train_single_scale(netD, netG, reals, Gs, Zs, in_s, NoiseAmp, opt, centers=N
             netG.zero_grad()
             output = netD(fake)
             
-            errG = -output.mean() 
+            loss_reconstruction = nn.MSELoss()(fake, real)
+            errG = -output.mean() + 0.1 * loss_reconstruction  # Aggiungi un termine di ricostruzione 
             errG1=errG.detach().requires_grad_(True)
             errG1.backward(retain_graph=True)
 
