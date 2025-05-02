9d0d6180b1d2fbdbe9fe23f54b82184c58f9cfe6
ponzuoligiacomo@gmail.com <ponzuoligiacomo@gmail.com>
2025-05-02 15:24:41 +0200

    fine tuning training

--- SinGAN/config.py
+++ SinGAN/config.py
@@ -30,12 +30,12 @@ def get_arguments():
     parser.add_argument('--max_size', type=int,help='image minimal size at the coarser scale', default=1280)
 
     #optimization hyper parameters:
-    parser.add_argument('--niter', type=int, default=2000, help='number of epochs to train per scale')
+    parser.add_argument('--niter', type=int, default=500, help='number of epochs to train per scale')
     parser.add_argument('--gamma',type=float,help='scheduler gamma',default=0.1)
-    parser.add_argument('--lr_g', type=float, default=0.0005, help='learning rate, default=0.0005')
-    parser.add_argument('--lr_d', type=float, default=0.0005, help='learning rate, default=0.0005')
+    parser.add_argument('--lr_g', type=float, default=0.002, help='learning rate, default=0.0005')
+    parser.add_argument('--lr_d', type=float, default=0.00001, help='learning rate, default=0.0005')
     parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
-    parser.add_argument('--Gsteps',type=int, help='Generator inner steps',default=3)
+    parser.add_argument('--Gsteps',type=int, help='Generator inner steps',default=5)
     parser.add_argument('--Dsteps',type=int, help='Discriminator inner steps',default=3)
     parser.add_argument('--lambda_grad',type=float, help='gradient penelty weight',default=0.1)
     parser.add_argument('--alpha',type=float, help='reconstruction loss weight',default=10)
