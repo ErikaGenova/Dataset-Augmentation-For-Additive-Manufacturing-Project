cb1ba9ab207a1b071846e141bde2a9ba67be06e3
ponzuoligiacomo@gmail.com <ponzuoligiacomo@gmail.com>
2025-05-03 15:54:30 +0200

    fine tuning training

--- SinGAN/config.py
+++ SinGAN/config.py
@@ -24,16 +24,16 @@ def get_arguments():
     parser.add_argument('--padd_size',type=int,help='net pad size',default=0)#math.floor(opt.ker_size/2)
         
     #pyramid parameters:
-    parser.add_argument('--scale_factor',type=float,help='pyramid scale factor',default=0.75)#pow(0.5,1/6))
+    parser.add_argument('--scale_factor',type=float,help='pyramid scale factor',default=0.90) # default 0.75
     parser.add_argument('--noise_amp',type=float, help='addative noise cont weight',default=0.1)
     parser.add_argument('--min_size',type=int,help='image minimal size at the coarser scale',default=25)
     parser.add_argument('--max_size', type=int,help='image minimal size at the coarser scale', default=1280)
 
     #optimization hyper parameters:
-    parser.add_argument('--niter', type=int, default=500, help='number of epochs to train per scale')
+    parser.add_argument('--niter', type=int, default=200, help='number of epochs to train per scale') # default 2000
     parser.add_argument('--gamma',type=float,help='scheduler gamma',default=0.1)
-    parser.add_argument('--lr_g', type=float, default=0.0002, help='learning rate, default=0.0005')
-    parser.add_argument('--lr_d', type=float, default=0.000001, help='learning rate, default=0.0005')
+    parser.add_argument('--lr_g', type=float, default=0.0005, help='learning rate, default=0.0005') # default 0.0005
+    parser.add_argument('--lr_d', type=float, default=0.0005, help='learning rate, default=0.0005') # default 0.0005
     parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
     parser.add_argument('--Gsteps',type=int, help='Generator inner steps',default=5)
     parser.add_argument('--Dsteps',type=int, help='Discriminator inner steps',default=3)
@@ -41,7 +41,7 @@ def get_arguments():
     parser.add_argument('--alpha',type=float, help='reconstruction loss weight',default=10)
     
     # gamma, weight fot the R1 penalty
-    parser.add_argument('--r1_penalty', type=str, help='R1 penalty', default='True')  
+    parser.add_argument('--r1_penalty', type=str, help='R1 penalty', default='False')  
     parser.add_argument('--lambda_r1',type=float, help='R1 penalty weight',default=10)
 
 
