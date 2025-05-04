872e687f4924f9353694c88f7f58c23a7706cabd
giorgiamodi <giorgia.modi@stud.unifi.it>
2025-05-04 18:43:08 +0200

    fix dimensions

--- external/PyTorch-GAN/implementations/dcgan/dcgan.py
+++ external/PyTorch-GAN/implementations/dcgan/dcgan.py
@@ -137,7 +137,7 @@ else:
 print(f"Number of images: {len(file_paths)}")
 
 transform=transforms.Compose(
-            [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
+            [transforms.Resize((opt.img_size, opt.img_size)), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
         )
 dataset = DefectDataset(file_paths, labels, transform=transform)
 dataloader = DataLoader(dataset, batch_size=opt.batch_size, shuffle=True)
