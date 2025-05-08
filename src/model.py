dee0c280d3a9351eb41613c3c0c3bfaf24356dd2
ponzuoligiacomo@gmail.com <ponzuoligiacomo@gmail.com>
2025-05-08 11:46:31 +0200

    enable the not pretrained mode

--- src/model.py
+++ src/model.py
@@ -1,5 +1,5 @@
 # builds & returns the CNN model
-
+import torch
 import torch.nn as nn
 import torchvision.models as models
 
@@ -8,7 +8,7 @@ def build_model(num_classes=2, backbone='resnet50', pretrained=False):
     '''Return a classification model with final layer adapted to num_classes.'''
     if backbone == 'resnet50':
         model = models.resnet50(weights=None if not pretrained else models.ResNet50_Weights.DEFAULT)
-        print("Weights model: ", model.conv1.weight)
+        #print("Weights model: ", model.conv1.weight)
 
         # Adapt first conv if grayscale input
         model.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
@@ -16,7 +16,7 @@ def build_model(num_classes=2, backbone='resnet50', pretrained=False):
         model.fc = nn.Linear(in_features, num_classes)
     elif backbone == 'resnet34':
         model = models.resnet34(weights=None if not pretrained else models.ResNet34_Weights.DEFAULT)
-        print("Weights model: ", model.conv1.weight)
+        #print("Weights model: ", model.conv1.weight)
 
         # Adapt first conv if grayscale input
         model.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
@@ -24,7 +24,7 @@ def build_model(num_classes=2, backbone='resnet50', pretrained=False):
         model.fc = nn.Linear(in_features, num_classes)
     elif backbone == 'resnet18':
         model = models.resnet18(weights=None if not pretrained else models.ResNet18_Weights.DEFAULT)
-        print("Weights model: ", model.conv1.weight)
+        #print("Weights model: ", model.conv1.weight)
 
         # Adapt first conv if grayscale input
         model.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
@@ -35,6 +35,8 @@ def build_model(num_classes=2, backbone='resnet50', pretrained=False):
     
 
     model_pretrained = build_model(num_classes=2, backbone='resnet18', pretrained=True)
-    print("\nPesi del modello pre-addestrato:")
-    print(model_pretrained.conv1.weight)
+    # Confronta i pesi del primo livello convoluzionale
+    are_weights_equal = torch.equal(model.conv1.weight, model_pretrained.conv1.weight)
+
+    print("\nI pesi del primo livello convoluzionale sono uguali?:", are_weights_equal)
     return model
