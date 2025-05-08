cd078308f22b2bfaab3d25ba1770826967aa108c
ponzuoligiacomo@gmail.com <ponzuoligiacomo@gmail.com>
2025-05-08 11:51:14 +0200

    enable the not pretrained mode

--- src/model.py
+++ src/model.py
@@ -7,24 +7,35 @@ import torchvision.models as models
 def build_model(num_classes=2, backbone='resnet50', pretrained=False):
     '''Return a classification model with final layer adapted to num_classes.'''
     if backbone == 'resnet50':
-        model = models.resnet50(weights=None if not pretrained else models.ResNet50_Weights.DEFAULT)
-        #print("Weights model: ", model.conv1.weight)
+        if pretrained == False:
+            print("No pretrained weights")
+            model = models.resnet50(weights=None)
+        else:
+            print("Pretrained weights")
+            model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
 
         # Adapt first conv if grayscale input
         model.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
         in_features = model.fc.in_features
         model.fc = nn.Linear(in_features, num_classes)
     elif backbone == 'resnet34':
-        model = models.resnet34(weights=None if not pretrained else models.ResNet34_Weights.DEFAULT)
-        #print("Weights model: ", model.conv1.weight)
-
+        if pretrained == False:
+            print("No pretrained weights")
+            model = models.resnet34(weights=None)
+        else:
+            print("Pretrained weights")
+            model = models.resnet50(weights=models.ResNet34_Weights.DEFAULT)
         # Adapt first conv if grayscale input
         model.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
         in_features = model.fc.in_features
         model.fc = nn.Linear(in_features, num_classes)
     elif backbone == 'resnet18':
-        model = models.resnet18(weights=None if not pretrained else models.ResNet18_Weights.DEFAULT)
-        #print("Weights model: ", model.conv1.weight)
+        if pretrained == False:
+            print("No pretrained weights")
+            model = models.resnet18(weights=None)
+        else:
+            print("Pretrained weights")
+            model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
 
         # Adapt first conv if grayscale input
         model.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
@@ -32,11 +43,5 @@ def build_model(num_classes=2, backbone='resnet50', pretrained=False):
         model.fc = nn.Linear(in_features, num_classes)
     else:
         raise ValueError(f"Unsupported backbone: {backbone}")
-    
 
-    model_pretrained = build_model(num_classes=2, backbone='resnet18', pretrained=True)
-    # Confronta i pesi del primo livello convoluzionale
-    are_weights_equal = torch.equal(model.conv1.weight, model_pretrained.conv1.weight)
-
-    print("\nI pesi del primo livello convoluzionale sono uguali?:", are_weights_equal)
     return model
