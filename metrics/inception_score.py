a359e97fbb3d6d48e7a04fdf82e3b9147f948a03
ponzuoligiacomo@gmail.com <ponzuoligiacomo@gmail.com>
2025-05-09 18:01:45 +0200

    add is

--- metrics/inception_score.py
+++ metrics/inception_score.py
@@ -82,7 +82,7 @@ def _init_inception():
     for op_idx, op in enumerate(ops):
         for o in op.outputs:
             shape = o.get_shape()
-            shape = [s.value for s in shape]
+            shape = [s.value if s is not None else None for s in shape]
             new_shape = []
             for j, s in enumerate(shape):
                 if s == 1 and j == 0:
