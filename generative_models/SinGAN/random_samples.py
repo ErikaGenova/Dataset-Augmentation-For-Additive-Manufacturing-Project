7e0d584ccacd7eeda1763d182620ecf1f8f5046f
ponzuoligiacomo@gmail.com <ponzuoligiacomo@gmail.com>
2025-05-21 16:37:06 +0200

    rename the folder

--- generative_models/SinGAN/random_samples.py
+++ generative_models/SinGAN/random_samples.py
@@ -1,7 +1,6 @@
 from config import get_arguments
 from SinGAN.manipulate import *
 from SinGAN.training import *
-from external.SinGAN.SinGAN.imresize import imresize
 import SinGAN.functions as functions
 
 
